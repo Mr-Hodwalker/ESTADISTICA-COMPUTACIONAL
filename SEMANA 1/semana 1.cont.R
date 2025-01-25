3efa58e02104ca4e5ac9f65900de85b6f1cf19ea
Mr-Hodwalker <hodwalkercarlos@gmail.com>
2025-01-25 13:21:03 -0500

    Create semana 1.cont.R

---
+++ SEMANA 1/semana 1.cont.R
@@ -0,0 +1,18 @@
+# ejemolo 1 ----
+x <- 3 
+if (x < 0) {
+  print('x es un numero negativo')
+} else { print('x es un numero positivo o cero')
+}
+
+# EJEMPLO 2 ----
+x <- 0 
+if (x < 0) {
+  print('x es un numero negativo')
+} else if (x==0){
+  print('x es cero')
+
+}else  { 
+  print('x es un numero positivo o cero')
+}
+
